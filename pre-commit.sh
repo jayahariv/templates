035a1c507b9ce55dd58309d78897af5900c2a612
Jayahari Vavachan <jayahariv88@gmail.com>
2018-08-03 09:41:42 -0700

    feat: conflict resolve

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-if git rev-parse --verify HEAD >/dev/null 2>&1
-then
-	against=HEAD
-else
-	# Initial commit: diff against an empty tree object
-	against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
-fi
 
 echo "checking pre-commit hooks..."
 diffstr=`git diff --cached $against | grep -e '^\+.*nocommit.*$'`
