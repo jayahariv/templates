0e2104f278acdfcd91ec674f0734bd0572c403b0
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2018-09-03 17:10:13 -0700

    feat: UIViewController Template
    * add my own template for UIViewController

---
+++ File Templates/Personal/UIKit Class.xctemplate/UIViewControllerSwift/___FILEBASENAME___.swift
@@ -0,0 +1,23 @@
+//___FILEHEADER___
+
+import UIKit
+
+final class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+
+    //MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+}
+
+private extension ___FILEBASENAMEASIDENTIFIER___ {
+    /**
+     configure UI once when view is loaded
+     
+     */
+    func configureUI() {
+        // implement
+    }
+}
